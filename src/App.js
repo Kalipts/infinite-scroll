aba5ce9e43494fad0e830508ec2d7ab9a29894c5
Nha Phan <nhaphan0073@gmail.com>
2020-04-13 10:14:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import './App.css';
 
+
+const style = {
+    height: 30,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8
+};
+
+
+
 function App() {
+  const [items, setItems] = useState( Array.from({length: 5}));
+
+  const fetchMoreData = () => {
+      setTimeout(()=> {
+          setItems(items.concat(Array.from({length: 5})))
+      }, 1500);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+       <div>
+           <InfiniteScroll
+               dataLength={items}
+               next={fetchMoreData}
+               hasMore={true}
+               loader={<h4>Loading...</h4>}
+           >
+               {items.map((i, index) => (
+                   <div style={style} key={index}>
+                       div - #{index}
+                   </div>
+               ))}
+           </InfiniteScroll>
+       </div>
+   </div>
   );
 }
 
